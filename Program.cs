a7d04a3c95dea214ea1433ce3977e2334ef3bf5c
Евгений Лактионов <laktionov_e@t.top-academy.ru>
2026-01-08 16:39:51 +0300

    Рефакторинг: методы

--- Program.cs
+++ Program.cs
@@ -1,96 +1,103 @@
 ﻿/*
-вывети заголовок программы
+ВывестиЗаголовокПрограммы(заголовок)
 создать генератор случайных чисел
-
 бесконечный цикл
-    случайное число от 1 до 3
-    если число = 1
-        ход компьютера = "к"
-    если число = 2
-        ход компьютера = "к"
-    если число = 3
-        ход компьютера = "б"
-
-    запросить ход игрока
-    ход игрока = считать строку
-    пока ход игрока не равен "к", "н", "б" или ""
-        вывести сообщение об ошибке
-        запросить ход игрока
-        ход игрока = считать строку
-    если ход игрока = пустая строка
+    ходКомпьютера = ВыполнитьХодКомпьютера
+    ходИгрока = ВыполнитьХодИгрока
+    если ходИгрока = пустая строка
         выйти из цикла
-   
-    если ход игрока = пустая строка
-        выйти из цикла    
-
-    вывести ход компьютера
-    вывести ход игрока
-
-    если ход игрока = ход компьютера
-        вывести "Ничья!"
-
-	если ход игрока = ход компьютера
-        вывести "Ничья!"
-    иначе если ход игрока = "к" и ход компьютера = "н"
-        вывести "Ты выиграл!"    
-    иначе если ход игрока = "н" и ход компьютера = "б"
-        вывести "Ты выиграл!"
-    иначе если ход игрока = "б" и ход компьютера = "к"
-        вывести "Ты выиграл!"
-    иначе вывести "Ты проиграл!"
-
-выйти из программы
+    победитель = ОпределитьПобедителя(ходИгрока, ходКомпьютера)
+    ВывестиПобедителя(ходКомпьютера, ходИгрока, победитель)
+ВыйтиИзПрограммы
 */
 
-Console.WriteLine("Камень, ножницы, бумага!");
-Console.WriteLine();
-
+WriteTitle("Камень, ножницы, бумага!");
 Random rand = new Random();
 
-while(true)
+while (true)
 {
-    string computerMove = "";
-    int computerNumber = rand.Next(1, 4);
-
-    if (computerNumber == 1)
-        computerMove = "к";
-    if(computerNumber == 2)
-        computerMove = "н";
-    if(computerNumber == 3)
-        computerMove = "б";
-
-    Console.Write("Твой ход (к/н/б): ");
-    string playerMove = Console.ReadLine();
-    while (playerMove != "к" 
-        && playerMove != "н" 
-        && playerMove != "б" 
-        && playerMove != "")
-    {
-        Console.WriteLine("Ошибка: неправильный ход!");
-        Console.Write("Твой ход (к/н/б): ");
-        playerMove = Console.ReadLine();
-    }
+    string computerMove = GetComputerMove();
+    string playerMove = GetPlayerMove();
+    playerMove = VerifyPlayerMove(playerMove);
     if (playerMove == "")
         break;
 
-    Console.WriteLine($"Компьютер: {computerMove}");
-    Console.WriteLine($"Игрок: {playerMove}");
+    string winner = GetWinner(playerMove, computerMove);
+    WriteWinner(computerMove, playerMove, winner);
+}
+ExitApp();
 
-    if (playerMove == computerMove)
-        Console.WriteLine("Ничья!");
-    else if (playerMove == "к" && computerMove == "н")
-        Console.WriteLine("Ты выиграл!");
-    else if (playerMove == "н" && computerMove == "б")
-        Console.WriteLine("Ты выиграл!");
-    else if (playerMove == "б" && computerMove == "к")
-        Console.WriteLine("Ты выиграл!");
-    else
-        Console.WriteLine("Ты проиграл!");
-
+void WriteTitle(string title)
+{
+    Console.WriteLine(title);
+    Console.WriteLine(new string('-', title.Length));
     Console.WriteLine();
 }
 
-Console.WriteLine();
-Console.Write("Для выхода нажми Enter");
-Console.ReadLine();
+string GetComputerMove()
+{
+    int computerNumber = rand.Next(1, 4);
+    if (computerNumber == 1)
+        return "к";
+    if (computerNumber == 2)
+        return "н";
+    if (computerNumber == 3)
+        return "б";
+    return "";
+}
 
+string GetPlayerMove()
+{
+    Console.Write("Твой ход (к/н/б): ");
+    string playerMove = Console.ReadLine();
+
+    return playerMove;
+}
+
+string VerifyPlayerMove(string playerMove)
+{
+    while (playerMove != "к"
+        && playerMove != "н"
+        && playerMove != "б"
+        && playerMove != "")
+    {
+        Console.WriteLine("Ошибка: неправильный ход!");
+        playerMove = GetPlayerMove();
+    }
+    return playerMove;
+}
+
+string GetWinner(string playerMove, string computerMove)
+{
+    if (playerMove == computerMove)
+        return "Ничья";
+    else if (playerMove == "к" && computerMove == "н")
+        return "Игрок";
+    else if (playerMove == "н" && computerMove == "б")
+        return "Игрок";
+    else if (playerMove == "б" && computerMove == "к")
+        return "Игрок";
+    else
+        return "Компьютер";
+}
+
+void WriteWinner(string computerMove, string playerMove, string winner)
+{
+    Console.WriteLine($"Компьютер: {computerMove}");
+    Console.WriteLine($"Игрок: {playerMove}");
+
+    if (winner == "Ничья")
+        Console.WriteLine("Ничья!");
+    else if (winner == "Игрок")
+        Console.WriteLine("Ты выиграл!");
+    else
+        Console.WriteLine("Ты проиграл!");
+    Console.WriteLine();
+}
+
+void ExitApp()
+{
+    Console.WriteLine();
+    Console.Write("Для выхода нажми Enter");
+    Console.ReadLine();
+}
