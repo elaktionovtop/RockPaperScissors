3e7a63d6179ffce6cec51c810cdb64473f2d9d88
Евгений Лактионов <laktionov_e@t.top-academy.ru>
2026-01-08 10:18:34 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿/*
+This is a simple C# program that prints "Rock, paper, scissors!" to the console
+ and waits for the user to press Enter before closing.
+*/
+
+Console.WriteLine("Rock, paper, scissors!");
+Console.WriteLine();
+
+Console.ReadLine();
+
